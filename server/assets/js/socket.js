47f07997fe54d758571f33fd67312f12e2f5fe17
indywka <min4ik.edward@gmail.com>
2018-06-25 23:41:08 +0300

    Revert "release v. 0.0.2"
    This reverts commit 5b5143d

--- server/assets/js/socket.js
+++
@@ -1,8 +0,0 @@
-io.socket.get('/connect', function onConnect (data) {
-  console.log("Socket connected!");
-});
-
-io.socket.on('switch', function (data) {
-  image = document.getElementById('slide');
-  image.src = "/images/"+data.howdy+".png";
-});
